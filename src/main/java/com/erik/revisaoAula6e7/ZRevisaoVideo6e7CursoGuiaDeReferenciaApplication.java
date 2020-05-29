296d79d860a0d9b1f8265a19cd739cbf9e3798d5
erikflima <erik.alves253@gmail.com>
2020-05-29 03:52:38 -0300

    First Commit

---
+++ src/main/java/com/erik/revisaoAula6e7/ZRevisaoVideo6e7CursoGuiaDeReferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.erik.revisaoAula6e7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZRevisaoVideo6e7CursoGuiaDeReferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZRevisaoVideo6e7CursoGuiaDeReferenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erik/revisaoAula6e7/ZRevisaoVideo6e7CursoGuiaDeReferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erik.revisaoAula6e7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZRevisaoVideo6e7CursoGuiaDeReferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
